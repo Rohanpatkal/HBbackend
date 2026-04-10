9f81621cb1da43ab21852d6f24025e347577fe92
Rohan Patkal <rohan.patkal@vernost.com>
2026-04-10 18:02:01 +0530

    alksjdlkfjlkasdjflkjasldkjflkas

--- services/common.js
+++ services/common.js
@@ -1,19 +1,59 @@
+import { log } from "console";
 import fs from "fs";
 
 const services = {
     dataBreaker: function (rowData) {
-        //freshly devide into month data
-        // "-----------------------------------------------------------------------"
-        //remove unessory comments from rowData
-        // console.log("rowDatarowData",rowData);
-        
-        let lines = rowData.split("-----------------------------------------------------------------------");
-        console.log("lineslineslines",lines);
-        
-        return "hello world";
+
+        const monthData = this.dataOparation(rowData);
+        fs.writeFileSync("data.json", JSON.stringify(monthData, null, 2));
+        return "File written successfully";
     },
-    monthBreaker: function (data) {
-        return data;
+    dataOparation: function (rowData) {
+        const monthData = {};
+
+        const trimedRowData = rowData.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, '').trim();
+        let yearRowData = trimedRowData.trim().split("-----------------------------------------------------------------------").map(line => line.trim()).filter(line => line.length > 0);
+
+        yearRowData.forEach(monthElement => {
+            let cleanMonthData = monthElement.trim();
+            cleanMonthData.split("\n").forEach(dayElement => {
+                let dayData = {
+                    count: 0,
+                    day: "", month: "", year: "",
+                    data: []
+                };
+                const singleDayBreak = dayElement.trim().split(/\s+/);
+                dayData.count = singleDayBreak[0];
+                const [day, month, year] = singleDayBreak[2].split("/");
+                dayData.day = day;
+                dayData.month = month;
+                dayData.year = year;
+                if (!monthData[`array-${month}-${year}`]) {
+                    monthData[`array-${month}-${year}`] = {
+                        monthDetails: {},
+                        dayArray: []
+                    };
+                }
+                monthData[`array-${month}-${year}`].dayArray.push(dayData);
+            })
+        });
+        return monthData;
+    },
+    totalMonthDetails: function (monthData) {
+        for (const monthKey in monthData) {
+            if (monthData.hasOwnProperty(monthKey)) {
+                const monthDetails = monthData[monthKey].monthDetails;
+                const dayArray = monthData[monthKey].dayArray;
+
+                let totalCount = 0;
+                dayArray.forEach(day => {
+                    totalCount += parseInt(day.count, 10);
+                });
+
+                monthDetails.totalCount = totalCount;
+            }
+        }
+        return monthData;
     }
 }
 export default services
